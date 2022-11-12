dba3261827241de0af4deedda0a4b5c47e8248a1
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2022-11-12 18:53:57 +0200

    Add files via upload

--- Lens Flare/lua/autorun/client/lensflare.lua
+++ Lens Flare/lua/autorun/client/lensflare.lua
@@ -1,38 +1,56 @@
-CreateClientConVar("snfl_enable", "1", true, false)
+local CVAREnabled = CreateClientConVar("snfl_enable", "1", true, false)
 
 --Floats
-CreateClientConVar("snfl_transparency", "1.2", true, false)
-CreateClientConVar("snfl_flareSize", "360", true, false)
-CreateClientConVar("snfl_spaceing", "1.2", true, false)
-CreateClientConVar("snfl_area", "0.2", true, false)
+local CVARTranspc = CreateClientConVar("snfl_transparency", "1.2", true, false)
+local CVARSize = CreateClientConVar("snfl_flareSize", "360", true, false)
+local CVARSpaceing = CreateClientConVar("snfl_spaceing", "1.2", true, false)
+local CVARArea = CreateClientConVar("snfl_area", "0.2", true, false)
 --Bools
-CreateClientConVar("snfl_rotate", "1", true, false)
-CreateClientConVar("snfl_fast", "1", true, false)
-CreateClientConVar("snfl_obstruction", "1", true, false)
+local CVARRotation = CreateClientConVar("snfl_rotate", "1", true, false)
+local CVARFastMode = CreateClientConVar("snfl_fast", "1", true, false)
+local CVARObstruct = CreateClientConVar("snfl_obstruction", "1", true, false)
 --Strings
-CreateClientConVar("snfl_sprites", "sprites/glow04_noz sprites/blueflare1_noz_gmod sprites/orangecore1_gmod sprites/light_ignorez", true, false)
-CreateClientConVar("snfl_sprites_angles", "0 0 0", true, false)
-CreateClientConVar("snfl_sprites_scales", "1 1 1", true, false)
+local CVARSpriteList = CreateClientConVar("snfl_sprites", "sprites/glow04_noz sprites/blueflare1_noz_gmod sprites/orangecore1_gmod sprites/light_ignorez", true, false)
+local CVARSpriteAngl = CreateClientConVar("snfl_sprites_angles", "0 0 0", true, false)
+local CVARSpriteScls = CreateClientConVar("snfl_sprites_scales", "1 1 1", true, false)
 --Ints
-CreateClientConVar("snfl_color_r", "255", true, false)
-CreateClientConVar("snfl_color_g", "255", true, false)
-CreateClientConVar("snfl_color_b", "255", true, false)
+local CVARcolorR = CreateClientConVar("snfl_color_r", "255", true, false)
+local CVARcolorG = CreateClientConVar("snfl_color_g", "255", true, false)
+local CVARcolorB = CreateClientConVar("snfl_color_b", "255", true, false)
 
-local convarstring = GetConVar("snfl_sprites"):GetString()
-local convarangles = GetConVar("snfl_sprites_angles"):GetString()
-local convarscales = GetConVar("snfl_sprites_scales"):GetString()
-
+local convarstring = CVARSpriteList:GetString()
+local convarangles = CVARSpriteAngl:GetString()
+local convarscales = CVARSpriteScls:GetString()
 
 local spritetbl = string.Explode( " ", convarstring )
 local anglestbl = string.Explode( " ", convarangles )
 local scalestbl = string.Explode( " ", convarscales )
 local FlareSprites = spritetbl
+local FlareMaterials = {}
 local FlareAngle = anglestbl
 local FlareScales = scalestbl
 local AppList = nil
-		cvars.AddChangeCallback("snfl_sprites", function(convar_name, value_old, value_new)
+
+local function RebuildMaterialTable()
+	FlareMaterials = {}
+    for i,v in ipairs(FlareSprites) do
+		local FlareMat = Material(v)
+		if FlareMat:IsError() then
+			chat.AddText( "Sun Flare material NOT found (" .. v .. ")" )
+		end
+		FlareMat:SetInt( "$additive", 1 )
+		FlareMaterials[i] = FlareMat
+    end
+end
+
+RebuildMaterialTable()
+
+cvars.AddChangeCallback("snfl_sprites", function(convar_name, value_old, value_new)
+
     spritetbl = string.Explode( " ", value_new )
     FlareSprites = spritetbl
+    RebuildMaterialTable()
+
     if !IsValid(AppList) then return end
     AppList:Clear()
     for k, v in ipairs (FlareSprites) do
@@ -48,6 +66,7 @@ cvars.AddChangeCallback("snfl_sprites_angles", function(convar_name, value_old, 
 		AppList:AddLine( k, v, FlareAngle[k], FlareScales[k] )
 	end
 end)
+
 cvars.AddChangeCallback("snfl_sprites_scales", function(convar_name, value_old, value_new)
     scalestbl = string.Explode( " ", value_new )
     FlareScales = scalestbl
@@ -57,134 +76,314 @@ cvars.AddChangeCallback("snfl_sprites_scales", function(convar_name, value_old, 
 		AppList:AddLine( k, v, FlareAngle[k], FlareScales[k] )
 	end
 end)
+
+local cmdstotable = {
+	"snfl_transparency",
+	"snfl_flareSize",
+	"snfl_spaceing",
+	"snfl_area",
+	"snfl_rotate",
+	"snfl_fast",
+	"snfl_obstruction",
+	"snfl_color_r",
+	"snfl_color_g",
+	"snfl_color_b",
+	"snfl_sprites",
+	"snfl_sprites_angles",
+	"snfl_sprites_scales"
+}
+
 function loadSNFLpreset( filePath )
 	local openFile = file.Read( filePath, "GAME" )
 	if !openFile then return end
 	local jtt = util.JSONToTable(openFile)
-	RunConsoleCommand("snfl_transparency", jtt[1])
-	RunConsoleCommand("snfl_flareSize", jtt[2])
-	RunConsoleCommand("snfl_spaceing", jtt[3])
-	RunConsoleCommand("snfl_area", jtt[4])
-	RunConsoleCommand("snfl_rotate", jtt[5])
-	RunConsoleCommand("snfl_fast", jtt[6])
-	RunConsoleCommand("snfl_obstruction", jtt[7])
-	RunConsoleCommand("snfl_color_r", jtt[8])
-	RunConsoleCommand("snfl_color_g", jtt[9])
-	RunConsoleCommand("snfl_color_b", jtt[10])
-	RunConsoleCommand("snfl_sprites", jtt[11])
-	RunConsoleCommand("snfl_sprites_angles", jtt[12])
-	RunConsoleCommand("snfl_sprites_scales", jtt[13])
+
+	for num,cmd in ipairs(cmdstotable) do
+		RunConsoleCommand(cmd, jtt[num])
+	end
 
 	surface.PlaySound( "garrysmod/content_downloaded.wav" )
 	chat.AddText( filePath .. ".json loaded" )
 end
 
+if !file.Exists( "snfl", "DATA" ) then
+	file.CreateDir("snfl")
+end
+
+if !file.Exists( "snfl/default_gmod.json", "DATA" ) then
+	print("snfl/default_gmod.json missing")
+	local defsnfl = {
+		15,
+		299,
+		0.439,
+		0.15,
+		0.0,
+		1.0,
+		1.0,
+		255,
+		255,
+		255,
+		"sprites/glow04_noz sprites/blueflare1_noz_gmod sprites/orangecore1_gmod sprites/light_ignorez",
+		"0 0 0 90",
+		"1 1 1 2"
+	}
+	local defwrite = util.TableToJSON( defsnfl )
+	file.Write("snfl/default_gmod.json", defwrite)
+end
+
+if !file.Exists( "snfl/default_snfl.json", "DATA" ) then
+	print("snfl/default_snfl.json missing")
+	local defsnfl = {
+	8.0,
+	235.0,
+	0.5,
+	0.15,
+	1.0,
+	1.0,
+	1.0,
+	255.0,
+	255.0,
+	255.0,
+	"effects/snfl/fx_flare1 effects/snfl/fx_flare2 effects/snfl/fx_flare3 effects/snfl/rainbow",
+	"0 0 0 105",
+	"1 1 1 2"
+	}
+	local defwrite = util.TableToJSON( defsnfl )
+	file.Write("snfl/default_snfl.json", defwrite)
+end
+
+local snflwhitelist = {
+	"default_gmod.json",
+	"default_snfl.json"
+}
+
+local function whiteList( filePath )
+	local stipedsnfl = string.GetFileFromFilename(filePath)
+	for k, v in ipairs(snflwhitelist)do
+		if stipedsnfl == v then
+			return false
+		end
+	end
+	return true
+end
+
 function deleteSNFLpreset( filePath )
 	local delfile = string.Replace(filePath, "data/", "")
-	file.Delete( delfile, "DATA" )
-	print(file.Exists( filePath, "GAME" ))
+	if whiteList( delfile ) then
+		file.Delete( delfile, "DATA" )
+	end
 	if file.Exists( filePath, "GAME" )then
 		surface.PlaySound( "common/wpn_denyselect.wav" )
+		chat.AddText( filePath .. " cannot be deleted" )
 	else
 		surface.PlaySound( "common/wpn_hudoff.wav" )
-		chat.AddText( filePath .. ".json deleted" )
+		chat.AddText( filePath .. " has been deleted" )
 	end
 end
 
-
-concommand.Add( "snfl_load", function( ply, cmd, args, str )
-	loadSNFLpreset( "data/" .. str )
-end, function(cmd, args)
-	local tbl = {}
-	local files, directories = file.Find( "data/snfl/*", "GAME" )
-	for k, v in ipairs(files) do
-		table.insert(tbl, "snfl_load snfl/" .. files[k])
+do
+	local function cbackfunc( ply, cmd, args, str )
+		loadSNFLpreset( "data/" .. str )
 	end
-	return tbl
+
+	local function AutoComplete(cmd, args)
+		local tbl = {}
+		local files, directories = file.Find( "data/snfl/*", "GAME" )
+		for k, v in ipairs(files) do
+			table.insert(tbl, "snfl_load snfl/" .. files[k])
+		end
+		return tbl
+	end
+
+	concommand.Add( "snfl_load", cbackfunc, AutoComplete)
+end
+
+local ENABLED = CVAREnabled:GetBool() -- snfl_enable
+
+local snfl_transparency = CVARTranspc:GetFloat() -- snfl_transparency
+local snfl_flareSize = CVARSize:GetFloat() -- snfl_flareSize
+local snfl_spaceing = CVARSpaceing:GetFloat() -- snfl_spaceing
+local snfl_area = CVARArea:GetFloat() -- snfl_area
+--Bools
+local snfl_rotate = CVARRotation:GetBool() -- snfl_rotate
+local snfl_fast = CVARFastMode:GetBool() -- snfl_fast
+local snfl_obstruction = CVARObstruct:GetBool() -- snfl_obstruction
+--Strings
+local snfl_sprites = CVARSpriteList:GetString() -- snfl_sprites
+local snfl_sprites_angles = CVARSpriteAngl:GetString() -- snfl_sprites_angles
+local snfl_sprites_scales = CVARSpriteScls:GetString() -- snfl_sprites_scales
+--Ints
+local snfl_color_r = CVARcolorR:GetInt() -- snfl_color_r
+local snfl_color_g = CVARcolorG:GetInt() -- snfl_color_g
+local snfl_color_b = CVARcolorB:GetInt() -- snfl_color_b
+
+local DrawColor = Color(255, 255, 255)
+
+
+local GetSunInfo = util.GetSunInfo
+local EyePos = EyePos
+local Clamp = math.Clamp
+
+local HalfVector = Vector(0.5, 0.5, 0)
+local SCRW = ScrW()
+local SCRH = ScrH()
+local ipairs = ipairs
+local errormat = Material( "icon16/error.png", "noclamp smooth" )
+
+local VectorMeta = FindMetaTable("Vector")
+
+local Length2DSqr = VectorMeta.Length2DSqr
+local Length2D = VectorMeta.Length2D
+local ToScreen = VectorMeta.ToScreen
+local VectorToAngle = VectorMeta.Angle
+
+hook.Add( "OnScreenSizeChanged", "LENSFLARE.SCR", function()
+	SCRW = ScrW()
+	SCRH = ScrH()
 end)
 
-hook.Add("PreDrawEffects", "PreDrawExample", function()
-	if !GetConVar("snfl_enable"):GetBool() then return end
-	if GetConVar("snfl_flareSize"):GetFloat() <= 0 then return end
-	if GetConVar("snfl_transparency"):GetFloat() <= 0 then return end
-	local fastCalc = GetConVar("snfl_flareSize"):GetBool()
-	local cone = GetConVar("snfl_area"):GetFloat()
-	local obstruction = GetConVar("snfl_obstruction"):GetBool()
-	local SunTable = util.GetSunInfo()
+local function DrawFunc()
+	if snfl_flareSize <= 0 then return end
+	if snfl_transparency <= 0 then return end
+
+	local obstruction = 1
+
+	local SunTable = GetSunInfo()
 	if !SunTable then return end
+
 	local SunBrightness = 1
 
-	if obstruction then
+	if snfl_obstruction then
 		if SunTable.obstruction == 0 then return end
 		obstruction = SunTable.obstruction
-	else
-		obstruction = 1
 	end
-	local SunToCenter = Vector(0.5, 0.5, 0)
-	local point = EyePos() + (SunTable.direction * 100)
-	local data2D = point:ToScreen()
+
+	local point = EyePos() + SunTable.direction
+
+	local data2D = ToScreen(point)
 	if ( !data2D.visible ) then return end
-	local SunCoords = Vector(data2D.x / ScrW(), data2D.y / ScrH(), 0)
-	SunToCenter = SunToCenter - Vector(data2D.x / ScrW(), data2D.y / ScrH(), 0)
 
-	if fastCalc then
-		SunBrightness = 1 - (SunToCenter:Length2DSqr() / cone)
+	local SunCoords = Vector(data2D.x / SCRW, data2D.y / SCRH, 0)
+
+	local SunToCenter = HalfVector - SunCoords
+
+	if snfl_fast then
+		SunBrightness = 1 - (Length2DSqr(SunToCenter) / snfl_area)
 	else
-		SunBrightness = 1 - (SunToCenter:Length2D() / cone)
+		SunBrightness = 1 - (Length2D(SunToCenter) / snfl_area)
 	end
 
-	SunBrightness = math.Clamp(SunBrightness, 0, 1) * obstruction
+	SunBrightness = Clamp(SunBrightness, 0, 1) * obstruction
 
-	if ((SunBrightness < 1 && fastCalc) || (SunBrightness > 0 && !fastCalc)) then
-		for k, v in ipairs(FlareSprites) do
-			local FlareMat = Material(v)
-			if FlareMat:IsError() then
-				cam.Start2D()
-						surface.SetDrawColor( 255, 255, 255, 255 )
-						surface.SetMaterial( Material( "icon16/error.png", "noclamp smooth" ) )
-						surface.DrawTexturedRect( 10, 10, 50 + math.sin(CurTime() * 5) * 2, 50 + math.sin(CurTime() * 5) * 2)
-
-						surface.SetFont( "Trebuchet24" )
-						surface.SetTextColor( 255, 255, 255 )
-						surface.SetTextPos( 80, 30 ) 
-						surface.DrawText( "One or more sun flare materials NOT found" )
-				cam.End2D()
-			 return end
-			FlareMat:SetInt( "$additive", 1 )
-			local spaceing = GetConVar("snfl_spaceing"):GetFloat()
-			local flareSize = GetConVar("snfl_flareSize"):GetFloat()
-			local direction = SunToCenter * (k * spaceing)
-			local flarePos = SunCoords + direction
-			flarePos = Vector(flarePos.x * ScrW(), flarePos.y * ScrH(), 0)
-			local clr = Color( GetConVar("snfl_color_r"):GetInt(), GetConVar("snfl_color_g"):GetInt(), GetConVar("snfl_color_b"):GetInt(), GetConVar("snfl_transparency"):GetFloat() * SunBrightness )
+	if SunBrightness > 0 then
+		for k, FlareMat in ipairs(FlareMaterials) do
+			local flarePos = SunCoords + SunToCenter * (k * snfl_spaceing)
+			flarePos = Vector(flarePos.x * SCRW, flarePos.y * SCRH, 0)
 			local angle = 0
 
-			if GetConVar("snfl_rotate"):GetBool() then
+			if snfl_rotate then
 				if FlareAngle[k] then
-					angle = -SunToCenter:Angle().y - tonumber(FlareAngle[k])
+					angle = -VectorToAngle(SunToCenter).y - FlareAngle[k]
 				else
-					angle = -SunToCenter:Angle().y
+					angle = -VectorToAngle(SunToCenter).y
 				end
 			else
 				if FlareAngle[k] then
-					angle = tonumber(FlareAngle[k])
+					angle = FlareAngle[k]
 				end
 			end
-
+			local flareSize = snfl_flareSize
 			if FlareScales[k] then
-				flareSize = flareSize * tonumber(FlareScales[k])
+				flareSize = snfl_flareSize * FlareScales[k]
 			end
-			cam.Start2D()
-				surface.SetDrawColor( clr )
-				surface.SetMaterial( Material(v) )
-				surface.DrawTexturedRectRotated( flarePos.x, flarePos.y, flareSize, flareSize, angle )
-			cam.End2D()
+
+			DrawColor.a = snfl_transparency * SunBrightness
+
+			surface.SetDrawColor( DrawColor )
+			surface.SetMaterial( FlareMat )
+			surface.DrawTexturedRectRotated( flarePos.x, flarePos.y, flareSize, flareSize, angle )
 		end
 	end
+end
+
+local function DisableLensFlares()
+	hook.Remove("RenderScreenspaceEffects", "CUSTOMIZABLELENSFL")
+end
+
+local function EnableLensFlares()
+	hook.Add("RenderScreenspaceEffects", "CUSTOMIZABLELENSFL", DrawFunc)
+end
+
+--[[-------------------------------------------------------------------------
+								Floats
+---------------------------------------------------------------------------]]
+cvars.AddChangeCallback("snfl_transparency", function(cname, old, new)
+	snfl_transparency = tonumber(new)
+end)
+cvars.AddChangeCallback("snfl_flareSize", function(cname, old, new)
+	snfl_flareSize = tonumber(new)
+end)
+cvars.AddChangeCallback("snfl_spaceing", function(cname, old, new)
+	snfl_spaceing = tonumber(new)
+end)
+cvars.AddChangeCallback("snfl_area", function(cname, old, new)
+	snfl_area = tonumber(new)
+end)
+--[[-------------------------------------------------------------------------
+								Bools
+---------------------------------------------------------------------------]]
+cvars.AddChangeCallback("snfl_rotate", function(cname, old, new)
+	snfl_rotate = tobool(new)
+end)
+
+cvars.AddChangeCallback("snfl_fast", function(cname, old, new)
+	snfl_fast = tobool(new)
+end)
+
+cvars.AddChangeCallback("snfl_obstruction", function(cname, old, new)
+	snfl_obstruction = tobool(new)
+end)
+--[[-------------------------------------------------------------------------
+								Strings
+---------------------------------------------------------------------------]]
+cvars.AddChangeCallback("snfl_sprites_angles", function(cname, old, new)
+	snfl_sprites_angles = tostring(new)
+end)
+
+cvars.AddChangeCallback("snfl_sprites_scales", function(cname, old, new)
+	snfl_sprites_scales = tostring(new)
+end)
+--[[-------------------------------------------------------------------------
+								Colors
+---------------------------------------------------------------------------]]
+cvars.AddChangeCallback("snfl_color_r", function(cname, old, new)
+	snfl_color_r = tonumber(new)
+	DrawColor.r = snfl_color_r
+end)
+
+cvars.AddChangeCallback("snfl_color_g", function(cname, old, new)
+	snfl_color_g = tonumber(new)
+	DrawColor.g = snfl_color_g
+end)
+
+cvars.AddChangeCallback("snfl_color_b", function(cname, old, new)
+	snfl_color_b = tonumber(new)
+	DrawColor.b = snfl_color_b
+end)
+--[[-------------------------------------------------------------------------
+								Enable/Disable
+---------------------------------------------------------------------------]]
+cvars.AddChangeCallback("snfl_enable", function(cname, old, new)
+    ENABLED = tobool(new)
+    if ENABLED then
+    	EnableLensFlares()
+    	return
+    end
+    DisableLensFlares()
 end)
 
 function SFAddControls()
-	spawnmenu.AddToolMenuOption( "Options", "Lens Flares", "FlareOptions", "#Sun Flares Settings", "", "", function( panel )
+	spawnmenu.AddToolMenuOption( "Options", "postprocessing", "SunFlares", "#Sun Flares", "", "", function( panel )
 		panel:ClearControls()
 
 		panel:CheckBox( "Enable Sun Flares", "snfl_enable")
@@ -291,10 +490,17 @@ function SFAddControls()
 			if !file.Exists( "snfl", "DATA" ) then
 				file.CreateDir("snfl")
 			end
-			surface.PlaySound( "buttons/button24.wav" )
-			chat.AddText( "snfl/" .. TextEntry:GetValue() .. ".json saved" )
-			file.Write( "snfl/" .. TextEntry:GetValue() .. ".json", tab)
-			browser:SetCurrentFolder( "" )
+
+			if whiteList( "snfl/" .. TextEntry:GetValue() .. ".json" ) then
+				file.Write( "snfl/" .. TextEntry:GetValue() .. ".json", tab)
+				surface.PlaySound( "buttons/button24.wav" )
+				chat.AddText( "snfl/" .. TextEntry:GetValue() .. ".json saved" )
+			else
+				surface.PlaySound( "common/wpn_denyselect.wav" )
+				chat.AddText( "snfl/" .. TextEntry:GetValue() .. ".json cannot be saved" )
+			end
+
+			browser:SetCurrentFolder( browser:GetCurrentFolder() )
 		end
 
 		browser.OnSelect = function( panel, filePath)
@@ -306,7 +512,7 @@ function SFAddControls()
 	end )
 end
 hook.Add( "AddToolMenuCategories", "CustomCategory", function()
-	spawnmenu.AddToolCategory( "Options", "Lens Flares", "#Lens Flares" )
+	spawnmenu.AddToolCategory( "Options", "postprocessing", "#spawnmenu.category.postprocess" )
 end )
 
 hook.Add( "PopulateToolMenu", "CustomMenuSettings", SFAddControls)
